a0adfa1bce7f3a1bf2bc71b8991955896eed4dea
Efandio <efandio904@gmail.com>
2025-08-01 20:26:56 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@
 function App() {
 
   return (
-    <>
+    <div className="w-screen h-screen bg-BackgroundLight flex">
       
-    </>
+      {/* Sidebar Container */}
+        <div>
+          
+        </div>
+
+
+      {/* Main Content Container */}
+        <div>
+
+        </div>
+    </div>
   )
 }
 
