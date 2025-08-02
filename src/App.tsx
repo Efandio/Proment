ac9aec26cc2c4280ab7545e966b7c3d9c3387aeb
Efandio <efandio904@gmail.com>
2025-08-02 10:07:40 +0700

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,27 @@ function App() {
   return (
     <div className="w-screen h-screen bg-BackgroundLight flex">
       
-      {/* Sidebar Container */}
-        <div>
+{/* Sidebar Container */}
+        <div className="w-[50vw] md:w-[40vw] lg:w-[15vw] h-full border border-r-LightStroke bg-CardAndSidebar">
           
         </div>
 
 
-      {/* Main Content Container */}
-        <div>
+{/* Main Content Container */}
+        <div className="w-full h-full">
+
+        {/* Header */}
+          <div className="w-full flex items-center">
+            
+          </div>
+
+        {/* Content */}
+          <div>
+            
+          </div>
 
         </div>
+
     </div>
   )
 }
