6d7e01136025f0a8d3cc030e73e021bfa9c3ea44
Efandio <efandio904@gmail.com>
2025-08-06 11:03:29 +0700

    button components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React, { useState } from "react";
 
 
 export interface ButtonInterface {
@@ -14,9 +14,23 @@ export type ButtonType = {
 
 type ButtonProps = ButtonType & ButtonInterface
 
-const MyButton: React.FC<ButtonProps> = ({ children, buttonType, onClick }) => {
+const MyButton: React.FC<ButtonProps> = ({ children, buttonType, size, variant, onClick }) => {
+
+    const variantClasses: Record<ButtonProps["variant"], string> = {
+        primary: "bg-PrimaryLight hover:bg-PrimaryLight/80",
+        "soft-primary": "bg-SoftPrimary hover:bg-SoftPrimary/80"
+    }
+
+    const sizeClasses: Record<ButtonProps["size"], string> = {
+        small: "px-4 py-0.5 text-xs",
+        "medium": "px-8 py-1",
+        "large": "px-12 py-1 text-xl",
+    }
+
     return (
-        <button type={buttonType} onClick={onClick} className={``}>
+        <button type={buttonType} onClick={onClick} className=
+        {`cursor-pointer rounded-md text-TextColorLightMode ${variantClasses[variant]} ${sizeClasses[size]}`}
+        >
             {children}
         </button>
     )
